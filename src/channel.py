7f3fa49744b2cf109ea6265d492ad63bb93edb5b
Alexander Kukin <KukinAU2008@mail.ru>
2024-03-01 20:48:46 +0400

    channel.py: реализована логика согласно hw2 main.py: Менять не нужно было, но сделал небольшие изменения, чтобы эксепшн не мешал проверить результат.

--- src/channel.py
+++ src/channel.py
@@ -1,6 +1,6 @@
 import json
 import os
-from googleapiclient.discovery import build
+from googleapiclient.discovery import build, Resource
 
 
 class Channel:
@@ -10,12 +10,38 @@ class Channel:
     api_key: str = os.getenv('YT_API_KEY')
 
     # создать специальный объект для работы с API
-    youtube = build('youtube', 'v3', developerKey=api_key)
+    youtube: Resource = build('youtube', 'v3', developerKey=api_key)
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
+        self.__channel_id = channel_id
+        self.url = os.path.join('https://www.youtube.com/channel/', channel_id)
+
         self.channel = Channel.youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
+
+        root_branch = self.channel['items'][0]
+        self.title = root_branch['snippet']['title']
+        self.description = root_branch['snippet']['description']
+
+        self.subscriber_cnt = root_branch['statistics']['subscriberCount']
+        self.video_cnt = int(root_branch['statistics']['videoCount'])
+        self.view_cnt = int(root_branch['statistics']['viewCount'])
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
         print(json.dumps(self.channel, indent=2, ensure_ascii=False))
+
+    @classmethod
+    def get_service(cls) -> Resource:
+        """Возвращает объект для работы с YouTube API"""
+        return Channel.youtube
+
+    def to_json(self, file_name):
+        """Cохраняет в файл значения атрибутов экземпляра Channel"""
+        with open(file_name, 'w', encoding='utf-8') as f:
+            json.dump(self.__dict__, f, indent=4)
+
+    @property
+    def channel_id(self):
+        """Геттер атрибуты channel_id"""
+        return self.__channel_id
